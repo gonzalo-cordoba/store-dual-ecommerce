2c1bed443e6b6ca38dbb63a5fb463a6133c56476
Gonzalo Cordoba <GonzaloCordoba070@gmail.com>
2024-06-07 16:34:38 -0300

    build(register): register.jsx
    Hice formulario para sección de Registrarse.

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -28,16 +28,33 @@ export const Register = () => {
               </label>
               <div className="mt-2">
                 <input
-                  id="name"
+                  id="text"
                   name="name"
-                  type="name"
-                  autoComplete="name"
+                  type="text"
+                  autoComplete="text"
                   required
-                  className="block w-full rounded-md border-0 py-1.5 text-white-900 shadow-sm ring-1 ring-inset ring-white-300 placeholder:text-white-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-white-300 placeholder:text-white-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
               </div>
             </div>
-
+            <div>
+              <label
+                htmlFor="email"
+                className="block text-sm font-medium leading-6 text-white-900"
+              >
+                Apellido
+              </label>
+              <div className="mt-2">
+                <input
+                  id="text"
+                  name="name"
+                  type="text"
+                  autoComplete="text"
+                  required
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-white-300 placeholder:text-white-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
             <div>
               <label
                 htmlFor="email"
@@ -65,14 +82,6 @@ export const Register = () => {
                 >
                   Contraseña
                 </label>
-                <div className="text-sm">
-                  <a
-                    href="#"
-                    className="font-semibold text-indigo-600 hover:text-indigo-500"
-                  >
-                    Olvidaste tu contraseña?
-                  </a>
-                </div>
               </div>
               <div className="mt-2">
                 <input
@@ -91,18 +100,18 @@ export const Register = () => {
                 type="submit"
                 className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
-                Iniciar sesión
+                Registrarse
               </button>
             </div>
           </form>
 
           <p className="mt-10 text-center text-sm text-gray-500">
-            ¿Aun no estas registrado?{" "}
+            ¿Ya tienes cuenta?{" "}
             <Link
-              to="/registrarse"
+              to="/login"
               className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
             >
-              Registrate
+              Inicia sesión
             </Link>
           </p>
         </div>
